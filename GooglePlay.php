267242b61415711b33cf99827a1e1b0cf6c9a653
Max Base <MaxBaseCode@Gmail.Com>
2020-10-19 15:38:27 +0330

    Create class

--- GooglePlay.php
+++ GooglePlay.php
@@ -2,3 +2,66 @@
 // Max Base
 // github.com/basemax/GooglePlayWebServiceAPI
 // 2020-10-19
+class GooglePlay {
+	private $debug=true;
+	private $categories=[
+		"app"=>[
+			"Art & Design",
+			"Augmented Reality",
+			"Auto & Vehicles",
+			"Beauty",
+			"Books & Reference",
+			"Business",
+			"Comics",
+			"Communication",
+			"Dating",
+			"Daydream",
+			"Education",
+			"Entertainment",
+			"Events",
+			"Finance",
+			"Food & Drink",
+			"Health & Fitness",
+			"House & Home",
+			"Libraries & Demo",
+			"Lifestyle",
+			"Maps & Navigation",
+			"Medical",
+			"Music & Audio",
+			"News & Magazines",
+			"Parenting",
+			"Personalization",
+			"Photography",
+			"Productivity",
+			"Shopping",
+			"Social",
+			"Sports",
+			"Tools",
+			"Travel & Local",
+			"Video Players & Editors",
+			"Wear OS by Google",
+			"Weather",
+		],
+		"game"=>[
+			"Action",
+			"Adventure",
+			"Arcade",
+			"Board",
+			"Card",
+			"Casino",
+			"Casual",
+			"Educational",
+			"Music",
+			"Puzzle",
+			"Racing",
+			"Role Playing",
+			"Simulation",
+			"Sports",
+			"Strategy",
+			"Trivia",
+			"Word",
+		],
+	];
+}
+$google = new GooglePlay();
+$google->parseApplication("com.bezapps.flowdiademo");
